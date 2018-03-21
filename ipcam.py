3d903d03290456545a1c7cc1f0aa946ec3e78afd
Noah Johnson <noah_johnson@my.uri.edu>
2018-03-21 16:00:21 -0400

    working ip cam with opencv2. uses request model.

--- ipcam.py
+++ ipcam.py
@@ -1,31 +1,20 @@
-# Stream Video with OpenCV from an Android running IP Webcam
-# (https://play.google.com/store/apps/details?id=com.pas.webcam)
-# Code Adopted from
-# http://stackoverflow.com/questions/21702477/how-to-parse-mjpeg-http-stream-from-ip-camera
-
 import cv2
-import urllib2
+import requests
 import numpy as np
-import sys
 
-host = "192.168.0.3:8080"
-if len(sys.argv) > 1:
-    host = sys.argv[1]
-
-hoststr = 'http://' + host + '/video'
-print('Streaming ' + hoststr)
-
-stream = urllib2.urlopen(hoststr)
-
-bytes = ''
-while True:
-    bytes += stream.read(1024)
-    a = bytes.find('\xff\xd8')
-    b = bytes.find('\xff\xd9')
-    if a != -1 and b != -1:
-        jpg = bytes[a:b+2]
-        bytes = bytes[b+2:]
-        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_COLOR)
-        cv2.imshow(hoststr, i)
-        if cv2.waitKey(1) == 27:
-            exit(0)
+r = requests.get('http://192.168.0.3:8080/video', auth=('user', 'password'), stream=True)
+if(r.status_code == 200):
+    bytes = bytes()
+    for chunk in r.iter_content(chunk_size=1024):
+        bytes += chunk
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            cv2.imshow('i', i)
+            if cv2.waitKey(1) == 27: #wait for escape keypress
+                exit(0)
+else:
+    print("Received unexpected status code {}".format(r.status_code))
